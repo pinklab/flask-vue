f0b104008629270a4cea0ab63412a413acc544ac
Geraldo Castro <victormatheuscastro@gmail.com>
2020-06-18 23:50:08 -0300

    Add vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import axiosPlugin from './plugins/axios';
+import vuetify from './plugins/vuetify';
 
 Vue.use(axiosPlugin);
 Vue.config.productionTip = false;
@@ -10,5 +11,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
